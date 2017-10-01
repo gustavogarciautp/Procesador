5e4d9480e9fd1f66208033fe6356b53f7f31ee4c
gustavogarciautp <32319990+gustavogarciautp@users.noreply.github.com>
2017-09-30 23:54:22 -0500

    Add files via upload

---
+++ Entrega 1/ALU.vhd
@@ -0,0 +1,51 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity ALU is
+    Port ( Oper1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           Oper2 : in  STD_LOGIC_VECTOR (31 downto 0);
+           ALUOP : in  STD_LOGIC_VECTOR (5 downto 0);
+           ALURESULT : out  STD_LOGIC_VECTOR (31 downto 0));
+end ALU;
+
+architecture Behavioral of ALU is
+
+begin
+
+process(Oper1,Oper2,ALUOP)
+begin
+
+case ALUOP is
+	when "000000"=> 
+		ALURESULT<=Oper1 and Oper2;
+	when "000001"=>
+		ALURESULT<=Oper1 and not Oper2;
+	when "000010"=>
+		ALURESULT<=Oper1 or Oper2;
+	when "000011"=> 
+		ALURESULT<=Oper1 or not Oper2;
+	when "000100"=>
+		ALURESULT<=Oper1 xor Oper2;
+	when "000101"=>
+		ALURESULT<=Oper1 xnor Oper2;
+	when "000110"=> 
+		ALURESULT<=Oper1+Oper2;
+	when "000111"=>
+		ALURESULT<=Oper1-Oper2;
+	when "001000"=> --SLL
+		ALURESULT<=std_logic_vector(unsigned(Oper1) sll conv_integer(oper2(4 downto 0)));
+	when "001001"=> --SRL
+		ALURESULT<=std_logic_vector(unsigned(Oper1) srl conv_integer(oper2(4 downto 0)));
+	when "001010"=> --SRA
+		ALURESULT<=To_StdLogicVector(to_bitvector(Oper1) sra conv_integer(oper2(4 downto 0)));
+	when others=>--"111111" Instrucciones no definidas
+		ALURESULT<=(others=>'0');
+end case;
+
+end process;
+
+
+end Behavioral;
+
---
+++ Entrega 1/CU_tb.vhd
@@ -0,0 +1,74 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY CU_tb IS
+END CU_tb;
+ 
+ARCHITECTURE behavior OF CU_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT CU
+    PORT(
+         OP : IN  std_logic_vector(1 downto 0);
+         OP3 : IN  std_logic_vector(5 downto 0);
+         ALUOP : OUT  std_logic_vector(5 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal OP : std_logic_vector(1 downto 0) := (others => '0');
+   signal OP3 : std_logic_vector(5 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal ALUOP : std_logic_vector(5 downto 0);
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: CU PORT MAP (
+          OP => OP,
+          OP3 => OP3,
+          ALUOP => ALUOP
+        );
+
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+	
+		---------------Instrucciones Aritmetico Logicas---------------
+		OP<="10";
+
+		OP3<="000001";--0. AND
+		wait for 10 ns;
+		OP3<="000101";--1. ANDN
+		wait for 10 ns;
+		OP3<="000010";--2. OR
+		wait for 10 ns;
+		OP3<="000110";--3. ORN
+		wait for 10 ns;
+		OP3<="000011";--4. XOR
+		wait for 10 ns;
+		OP3<="000111";--5. XNOR
+		wait for 10 ns;
+		OP3<="000000";--6. ADD
+		wait for 10 ns;
+		OP3<="000100";--7. SUB
+		wait for 10 ns;
+		OP3<="100101";--8. SLL
+		wait for 10 ns;
+		OP3<="100110";--9. SRL
+		wait for 10 ns;
+		OP3<="100111";--10.SRA
+		wait for 10 ns;
+		
+		-----------------Otras Instrucciones(aun no definidas)-----------------------
+		OP<="00";
+		OP3<="010101";
+		
+      wait;
+   end process;
+
+END;
